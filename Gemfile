e4c132de20c16c0753969388682d571bb1923ca4
Dan Frenette <paradox815@gmail.com>
2015-02-25 13:15:09 -0500

    Create testing environment

--- Gemfile
+++ Gemfile
@@ -8,3 +8,14 @@ gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+
+group :development, :test do
+  gem 'guard'
+  gem 'guard-livereload'
+  gem 'guard-rspec'
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'shoulda-matchers'
+end
---
+++ spec/features/account_creation_feature_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+describe "account creation" do
+  it "allows a user to create an account" do
+    visit root_path
+    click_link "Create Account"
+
+    fill_in "Name", with: "Dan"
+    fill_in "Email", with: "Dan.R.Frenette@gmail.com"
+    fill_in "Password", with: "pw"
+    fill_in "Password Confirmation", with: "pw"
+    fill_in "Subdomain", with: "test_subdomain"
+    click_button "Create Account"
+
+    expect(page).to have_content("Signed up successfully!")
+  end
+end
